7cc698806c3282fc633c4a4fae195acd5d0d8068
Sahil <sahilsingh90@gmail.com>
2022-06-20 22:46:38 +0530

    Implement minimal key logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use eframe::egui::{self, Event};
+
 fn main() {
-    println!("Hello, world!");
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Key Shredder",
+        options,
+        Box::new(|_cc| Box::new(KeyShredder::default())),
+    );
 }
+
+#[derive(Default)]
+struct KeyShredder {
+}
+
+impl eframe::App for KeyShredder {
+     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+         let keys_down = ctx.input().keys_down.clone();
+
+         println!("Keys pressed: {:?}", &keys_down);
+     }
+}
+
