684f3522a442a418d6c276e6e1749ebfe8b388e3
Sahil <sahilsingh90@gmail.com>
2022-06-21 22:15:47 +0530

    Remove app impl block from main

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,8 @@
-use eframe::egui::{self, Event, Key, Modifiers};
-
 fn main() {
     let options = eframe::NativeOptions::default();
     eframe::run_native(
         "Key Shredder",
         options,
-        Box::new(|_cc| Box::new(KeyShredder::default())),
+        Box::new(|cc| Box::new(key_shredder::KeyShredder::new(cc))),
     );
-}
-
-#[derive(Default)]
-struct KeyShredder {
-}
-
-impl eframe::App for KeyShredder {
-     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        for e in ctx.input().events.iter() {
-            match *e {
-                Event::Key { key, pressed, modifiers } if pressed == true => {
-                    println!("{:?}, {:?}, {:?}", key, pressed, modifiers);
-                },
-                _ => {},
-            }
-        }
-     }
 }
