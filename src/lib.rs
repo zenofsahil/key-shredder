af7c632da7bab0342b58fb8ef9cfeb42ba899ddf
Sahil <sahilsingh90@gmail.com>
2022-06-21 22:39:36 +0530

    Better keyboard display

--- src/lib.rs
+++ src/lib.rs
@@ -24,27 +24,50 @@ impl Keyboard {
     }
 
     fn draw(&self, ui: &mut egui::Ui) {
-        egui::Frame::none().show(ui, |ui| {
-            ui.horizontal(|ui| {
-                for key in &self.first_row {
-                    egui::Frame::none().fill(BLACK).show(ui, |ui| {
-                        ui.colored_label(WHITE, format!("{:?}", key));
-                    });
-                }
+        egui::Frame::none()
+        .inner_margin(egui::style::Margin::symmetric(ui.available_width() / 3.0 , 0.0))
+        .show(ui, |ui| {
+            egui::Frame::none().inner_margin(egui::style::Margin::symmetric(10., 0.)).show(ui, |ui| {
+                ui.spacing_mut().item_spacing = (10.0, 0.0).into();
+                ui.horizontal(|ui| {
+                    for key in &self.first_row {
+                        egui::Frame::none().fill(BLACK).show(ui, |ui| {
+                            ui.label(
+                                egui::RichText::new(format!("{:?}", key))
+                                .size(20.)
+                                .color(Color32::WHITE)
+                            )
+                        });
+                    }
+                });
             });
-            ui.horizontal(|ui| {
-                for key in &self.second_row {
-                    egui::Frame::none().fill(BLACK).show(ui, |ui| {
-                        ui.colored_label(WHITE, format!("{:?}", key));
-                    });
-                }
+            egui::Frame::none().inner_margin(egui::style::Margin::symmetric(30., 0.)).show(ui, |ui| {
+                ui.spacing_mut().item_spacing = (10.0, 0.0).into();
+                ui.horizontal(|ui| {
+                    for key in &self.second_row {
+                        egui::Frame::none().fill(BLACK).show(ui, |ui| {
+                            ui.label(
+                                egui::RichText::new(format!("{:?}", key))
+                                .size(20.)
+                                .color(Color32::WHITE)
+                            )
+                        });
+                    }
+                });
             });
-            ui.horizontal(|ui| {
-                for key in &self.third_row {
-                    egui::Frame::none().fill(BLACK).show(ui, |ui| {
-                        ui.colored_label(WHITE, format!("{:?}", key));
-                    });
-                }
+            egui::Frame::none().inner_margin(egui::style::Margin::symmetric(40., 0.)).show(ui, |ui| {
+                ui.horizontal(|ui| {
+                    ui.spacing_mut().item_spacing = (10.0, 0.0).into();
+                    for key in &self.third_row {
+                        egui::Frame::none().fill(BLACK).show(ui, |ui| {
+                            ui.label(
+                                egui::RichText::new(format!("{:?}", key))
+                                .size(20.)
+                                .color(Color32::WHITE)
+                            )
+                        });
+                    }
+                });
             });
         });
     }
