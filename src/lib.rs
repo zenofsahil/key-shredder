861060703f29290b593ce419bb15f7e02854fc2f
Sahil <sahilsingh90@gmail.com>
2022-06-25 21:52:32 +0530

    Implement basic syntax highlighting using layout jobs

--- src/lib.rs
+++ src/lib.rs
@@ -18,10 +18,58 @@ impl eframe::App for KeyShredder {
             })
         });
         egui::CentralPanel::default().show(ctx, |ui| {
+
+            let mut layouter = |ui: &egui::Ui, string: &str, wrap_width: f32| {
+                use eframe::epaint::{Color32, text::{LayoutJob, TextFormat}, FontFamily, FontId};
+
+                let mut job = LayoutJob::default();
+                job.wrap.max_width = wrap_width;
+
+                for word in string.split(" ") {
+                    match word {
+                        "hello" => {
+                            job.append(
+                                &format!("{} ", word),
+                                0.0,
+                                TextFormat {
+                                    font_id: FontId::new(14.0, FontFamily::Monospace),
+                                    color: Color32::YELLOW,
+                                    ..Default::default()
+                                },
+                            );
+                        },
+                        "world" => {
+                            job.append(
+                                &format!("{} ", word),
+                                0.0,
+                                TextFormat {
+                                    font_id: FontId::new(14.0, FontFamily::Monospace),
+                                    color: Color32::GREEN,
+                                    ..Default::default()
+                                },
+                            );
+                        },
+                        _ => {
+                            job.append(
+                                &format!("{} ", word),
+                                0.0,
+                                TextFormat {
+                                    font_id: FontId::new(14.0, FontFamily::Monospace),
+                                    color: Color32::WHITE,
+                                    ..Default::default()
+                                },
+                            );
+                        }
+                    }
+                }
+
+                ui.fonts().layout_job(job)
+            };
             ui.add(
                 egui::TextEdit::multiline(&mut self.text)
                 .desired_width(f32::INFINITY)
                 .hint_text("Enter you text here")
+                .layouter(&mut layouter)
             );
         });
         let events = ctx.input().events.clone(); // clone to avoid deadlock
@@ -59,7 +107,7 @@ impl KeyShredder {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         Self {
             keyboard: Keyboard::default(),
-            text: String::from("Shred away")
+            text: String::new()
         }
     }
 }
